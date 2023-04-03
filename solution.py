e4f35bff87886ea7f169f5c31f22c2ba2918b4f6
Garbakbl <121131132+Garbakbl@users.noreply.github.com>
2023-04-03 19:43:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,8 @@ chat_id = 356550601
 
 def solution(p, x):
     n = len(x)
-    s = np.sqrt(np.sum(x**2)/(n-1))
+    s = np.sqrt(np.sum(x**2) / (n - 1))
     alpha = 1 - p
-    left = np.sqrt((n-1)*s**2/chi2.ppf(1-alpha/2, n-1))
-    right = np.sqrt((n-1)*s**2/chi2.ppf(alpha/2, n-1))
+    left = np.sqrt((n - 1) * s**2 / chi2.ppf(1 - alpha / 2, n-1)) / 35 
+    right = np.sqrt((n - 1) * s**2 / chi2.ppf(alpha / 2, n-1)) / 35
     return (left, right)
