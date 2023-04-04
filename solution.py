33bbf61818d2aa9a240943de0cc6a895fba61408
Garbakbl <121131132+Garbakbl@users.noreply.github.com>
2023-04-04 23:19:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,9 +4,8 @@ from scipy.stats import chi2
 chat_id = 356550601
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    s = np.sqrt(np.sum(x**2) / (n - 1))
     alpha = 1 - p
-    left = np.sqrt((n - 1) * s**2 / chi2.ppf(1 - alpha / 2, n-1) / 35)
-    right = np.sqrt((n - 1) * s**2 / chi2.ppf(alpha / 2, n-1) / 35)
+    size = len(x)
+    left = np.sqrt(size * (x ** 2).mean() / (35 * chi2.ppf(q=1 - alpha / 2, df=2 * size)))
+    right = np.sqrt(size * (x ** 2).mean() / (35 * chi2.ppf(q=alpha / 2, df=2 * size)))
     return (left, right)
